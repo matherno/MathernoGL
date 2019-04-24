8deffddb47b0da520c355bff25aba38aacfbed18
matherno <matthew520@live.com.au>
2019-04-24 21:15:13 +0800

    Refactor build script to build and distribute both debug and release
    This is so applications can have both debug and release versions of this library.
    So they should link to the appropriate one according to their build configuration

--- extract_includes.bash
+++ extract_includes.bash
@@ -5,3 +5,6 @@ rm -rf ./include
 mkdir -p ./include
 cp -r ./src/* ./include
 find ./include -type f -name '*.cpp' -delete
+
+cp ./bin_release/libMathernoGL.a ./include
+cp ./bin_debug/libMathernoGL.a ./include/libMathernoGL_DEBUG.a
